fe137bea7f663d96020c833c5f81c92f578bb3a9
parth2282001 <108646682+raja2282001@users.noreply.github.com>
2024-06-02 18:29:59 +0530

    init

--- php/contactForm.php
+++ php/contactForm.php
@@ -1,26 +1,27 @@
-<?
-$name  = $_REQUEST["name"];
-$email = $_REQUEST["email"];
-$subject = $_REQUEST["subject"];
-$msg   = $_REQUEST["msg"];
-$to    = "patelparth4655@gmail.com"; // ENTER YOUR EMAIL ADDRESS
-if (isset($email) && isset($name) && isset($msg)) {
-    $email_subject = "$name sent you a message via YOUR SITE NAME"; // ENTER YOUR EMAIL SUBJECT
-		$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
-$headers .= "From: ".$name." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
-$msg     = "From: $name<br/> Email: $email <br/> Subject: $subject <br/> Message: $msg";
-	
-   $mail =  mail($to, $email_subject, $msg, $headers);
-  if($mail)
-	{
-		echo 'success';
-	}
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents("php://input"), true);
+    
+    $name = $data["name"];
+    $email = $data["email"];
+    $subject = $data["subject"];
+    $msg = $data["msg"];
+    $to = "patelparth4655@gmail.com";
 
-else
-	{
-		echo 'failed';
-	}
+    if (!empty($email) && !empty($name) && !empty($msg)) {
+        $email_subject = "$name sent you a message via YOUR SITE NAME";
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+        $headers .= "From: " . $name . " <" . $email . ">\r\n" . "Reply-To: " . $email . "\r\n";
+        $message = "From: $name<br/> Email: $email <br/> Subject: $subject <br/> Message: $msg";
+
+        if (mail($to, $email_subject, $message, $headers)) {
+            echo 'success';
+        } else {
+            echo 'failed';
+        }
+    } else {
+        echo 'failed';
+    }
 }
-
 ?>
